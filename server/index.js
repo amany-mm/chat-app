444975350d8a7a4a230918c41dc9a4b2eba54630
Amany Mohammed <amany.mohammd@gmail.com>
2025-01-22 09:04:04 +0200

    feat: Backend basic setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+require("dotenv").config();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+try {
+  mongoose.connect(process.env.MONGO_URL);
+  console.log('DB connection successful');
+} catch (err) {
+  console.error(err);
+}
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${server.address().port}`);
+});
