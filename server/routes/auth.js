bcb291b5a2cbd3be9beb271947f6d62a2bb24190
Amany Mohammed <amany.mohammd@gmail.com>
2025-01-25 15:09:09 +0200

    feat: Add set avatar/profile picture functionality.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,13 @@
 const {
   login,
   register,
+  setAvatar,
 } = require("../controllers/userController");
 
 const router = require("express").Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/setavatar/:id", setAvatar);
 
 module.exports = router;
